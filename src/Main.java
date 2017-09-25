3894d2222bcec82d39f249f4c60d00499b666134
cchs91419 <cchs91419@gmail.com>
2017-09-25 16:01:06 +0800

    20170925

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scn  = new Scanner(System.in);
-        int val1 = scn.nextInt();
-        float val2 = (float) 0.26418;
-        System.out .println(Math.round(val1*val2*10/10f));
+       Scanner scn = new Scanner(System.in);
+       int val1 = scn.nextInt();
+       float val2 =600;
+       System.out.println(Math.round(val1/val2*10/10f));
 
     }
 }
